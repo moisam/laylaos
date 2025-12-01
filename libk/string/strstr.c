5090aaea4cfaf6027e2b010a2e39462e0d2cfd81
moisam <34644392+moisam@users.noreply.github.com>
2025-12-01 15:53:23 +0000

    Add files via upload

--- libk/string/strstr.c
+++ libk/string/strstr.c
@@ -2,21 +2,28 @@
 
 char *strstr(const char *str1, const char *str2)
 {
-  const char *mystr1 = (const char *)str1;
-  const char *mystr2 = (const char *)str2;
-  do
-  {
-    if(*mystr1 == *mystr2)
+    const char *mystr1 = (const char *)str1;
+    const char *mystr2 = (const char *)str2;
+
+    do
     {
-      const char *str3 = (const char *)mystr1;
-      while(*mystr1 == *mystr2 && *mystr2)
-      {
-	mystr2++; mystr1++;
-      }
-      if(!*mystr2)
-	return (char *)str3;
-    }
-    mystr1++;
-  } while(*mystr1 && *mystr2);
-  return 0;
+        if(*mystr1 == *mystr2)
+        {
+            const char *str3 = (const char *)mystr1;
+
+            while(*mystr1 == *mystr2 && *mystr2)
+            {
+                mystr2++; mystr1++;
+            }
+
+            if(!*mystr2) return (char *)str3;
+
+            mystr2 = (const char *)str2;
+        }
+
+        mystr1++;
+    } while(*mystr1 && *mystr2);
+
+    return 0;
 }
+
