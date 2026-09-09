99cd60c77c42343842793d35aa199e0ad04c8d0f
Mohammed Isam <34644392+moisam@users.noreply.github.com>
2026-09-09 22:24:10 +0100

    Add files via upload

--- kernel/dev/blk/blk_rw.c
+++ kernel/dev/blk/blk_rw.c
@@ -1,6 +1,6 @@
 /* 
  *    Programmed By: Mohammed Isam [mohammed_isam1984@yahoo.com]
- *    Copyright 2021, 2022, 2023, 2024, 2025 (c)
+ *    Copyright 2021, 2022, 2023, 2024, 2025, 2026 (c)
  * 
  *    file: blk_rw.c
  *    This file is part of LaylaOS.
@@ -22,7 +22,7 @@
 /**
  *  \file blk_rw.c
  *
- *  General read and write functions for block memory devices.
+ *  General read and write functions for block devices.
  */
 
 #include <errno.h>
@@ -39,9 +39,6 @@
 ssize_t block_write(struct file_t *f, off_t *pos,
                     unsigned char *buf, size_t count, int kernel)
 {
-    //struct mount_info_t *d;
-    //struct cached_page_t *dbuf;
-    //struct fs_node_header_t tmpnode;
     struct disk_req_t req;
     dev_t dev = f->node->blocks[0];
     size_t blockno, off, i, done;
@@ -49,6 +46,9 @@ ssize_t block_write(struct file_t *f, off_t *pos,
     int blocksz = 0;
     int maj = MAJOR(dev);
     char *p, *tmpbuf;
+
+    //switch_tty(1);
+    //printk("block_write: %d:%d: count %lu, *pos %ld\n", maj, MINOR(dev), count, *pos);
 
     if(!pos || !buf)
     {
@@ -126,48 +126,6 @@ ssize_t block_write(struct file_t *f, off_t *pos,
 
     kfree(tmpbuf);
 
-#if 0
-    if((d = get_mount_info(dev)) == NULL)
-    {
-        return -EINVAL;
-    }
-
-    blockno = *pos / d->block_size;
-    off = *pos % d->block_size;
-    done = 0;
-    tmpnode.dev = dev;
-    tmpnode.inode = PCACHE_NOINODE;
-
-    while(count != 0)
-    {
-        if(!(dbuf = get_cached_page((struct fs_node_t *)&tmpnode, blockno, 0)))
-        {
-            return done ? (int)done : -EIO;
-        }
-        
-        i = (count < d->block_size) ? count : d->block_size;
-        p = (char *)(dbuf->virt + off);
-        off = 0;
-        blockno++;
-        *pos += i;
-        done += i;
-        count -= i;
-
-        if(kernel)
-        {
-            A_memcpy(p, buf, i);
-        }
-        else
-        {
-            copy_from_user(p, buf, i);
-        }
-
-        buf += i;
-        dbuf->flags |= PCACHE_FLAG_DIRTY;
-        release_cached_page(dbuf);
-    }
-#endif
-
     return done;
 }
 
@@ -178,9 +136,6 @@ ssize_t block_write(struct file_t *f, off_t *pos,
 ssize_t block_read(struct file_t *f, off_t *pos,
                    unsigned char *buf, size_t count, int kernel)
 {
-    //struct mount_info_t *d;
-    //struct cached_page_t *dbuf;
-    //struct fs_node_header_t tmpnode;
     struct disk_req_t req;
     size_t blockno, off, i, done;
     dev_t dev = f->node->blocks[0];
@@ -188,6 +143,9 @@ ssize_t block_read(struct file_t *f, off_t *pos,
     int blocksz = 0;
     int maj = MAJOR(dev);
     char *p, *tmpbuf;
+
+    //switch_tty(1);
+    //printk("block_read: %d:%d: count %lu, *pos %ld\n", maj, MINOR(dev), count, *pos);
 
     // make sure we have a strategy function
     if(!bdev_tab[maj].strategy)
@@ -249,47 +207,6 @@ ssize_t block_read(struct file_t *f, off_t *pos,
     }
 
     kfree(tmpbuf);
-
-#if 0
-    if((d = get_mount_info(dev)) == NULL)
-    {
-        return -EINVAL;
-    }
-
-    blockno = *pos / d->block_size;
-    off = *pos % d->block_size;
-    read = 0;
-    tmpnode.dev = dev;
-    tmpnode.inode = PCACHE_NOINODE;
-
-    while(count != 0)
-    {
-        if(!(dbuf = get_cached_page((struct fs_node_t *)&tmpnode, blockno, 0)))
-        {
-            return read ? (int)read : -EIO;
-        }
-
-        i = (count < d->block_size) ? count : d->block_size;
-        p = (char *)(dbuf->virt + off);
-        off = 0;
-        blockno++;
-        *pos += i;
-        read += i;
-        count -= i;
-
-        if(kernel)
-        {
-            A_memcpy(buf, p, i);
-        }
-        else
-        {
-            copy_to_user(buf, p, i);
-        }
-
-        buf += i;
-        release_cached_page(dbuf);
-    }
-#endif
 
     return done;
 }
